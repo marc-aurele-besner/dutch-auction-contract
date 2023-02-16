6531baf336c96c03ddcbed56ccec28b2649c4294
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2023-02-15 20:27:02 -0500

    improve deployment

--- scripts/deploy-DutchAuction.js
+++ scripts/deploy-DutchAuction.js
@@ -1,17 +1,22 @@
+const { upgrades } = require('hardhat');
 const hre = require('hardhat');
 
 async function main() {
     const [deployer] = await hre.ethers.getSigners();
-    // Set the token address to use for payment (ERC20)
-    const token = ethers.constants.AddressZero;
+
+    const MockERC20 = await hre.ethers.getContractFactory('MockERC20');
+    const mockERC20 = await MockERC20.deploy();
+    await mockERC20.deployed();
+
+    await hre.addressBook.saveContract('MockERC20', mockERC20.address, hre.network.name, deployer.address);
+    console.log('MockERC20 deployed to:', mockERC20.address);
 
     const DutchAuction = await hre.ethers.getContractFactory('DutchAuction');
-    const dutchAuction = await DutchAuction.deploy();
+    const dutchAuction = await upgrades.deployProxy(DutchAuction, [mockERC20.address], { initializer: 'initialize' });
 
     await dutchAuction.deployed();
-    await dutchAuction.initialize(token);
-    await hre.addressBook.saveContract('DutchAuction', dutchAuction.address, hre.network.name, deployer.address);
 
+    await hre.addressBook.saveContract('DutchAuction', dutchAuction.address, hre.network.name, deployer.address);
     console.log('DutchAuction deployed to:', dutchAuction.address);
 }
 
